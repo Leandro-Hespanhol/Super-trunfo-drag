20d79aa828552dfc834db73b6737559850c3fca9
Leandro Hespanhol <leandrohespanhol@gmail.com>
2022-05-29 12:48:31 -0300

    initializing

---
+++ src/cardsData/dragsData.js
@@ -0,0 +1,58 @@
+const drags = {
+  jinkx: {
+    charisma: 0,
+    uniqueness: 0,
+    nerve: 0,
+    talent: 0,
+  },
+  jinkx1: {
+    charisma: 0,
+    uniqueness: 0,
+    nerve: 0,
+    talent: 0,
+  },
+  jinkx2: {
+    charisma: 0,
+    uniqueness: 0,
+    nerve: 0,
+    talent: 0,
+  },
+  jinkx3: {
+    charisma: 0,
+    uniqueness: 0,
+    nerve: 0,
+    talent: 0,
+  },
+  jinkx4: {
+    charisma: 0,
+    uniqueness: 0,
+    nerve: 0,
+    talent: 0,
+  },
+  jinkx5: {
+    charisma: 0,
+    uniqueness: 0,
+    nerve: 0,
+    talent: 0,
+  },
+  jinkx6: {
+    charisma: 0,
+    uniqueness: 0,
+    nerve: 0,
+    talent: 0,
+  },
+  jinkx7: {
+    charisma: 0,
+    uniqueness: 0,
+    nerve: 0,
+    talent: 0,
+  },
+  jinkx8: {
+    charisma: 0,
+    uniqueness: 0,
+    nerve: 0,
+    talent: 0,
+  },
+};
+
+export default drags;
